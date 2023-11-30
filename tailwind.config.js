3a33960d6fdedb273311148af61ecdf6e2e830f9
Yogendra Rana <yogendrarana4321@gmail.com>
2023-11-30 15:02:08 +0545

    Changed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,17 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {
+        content: {
             backgroundImage: {
                 'formImage': 'url("./src/assets/bg.jpg")',
-            }
+            },
+        },
+
+        screens: {
+            'xl': { 'max': '1280px' },
+            'lg': { 'max': '1024px' },
+            'md': { 'max': '768px' },
+            'sm': { 'max': '480px' },
         },
     },
     plugins: [],
